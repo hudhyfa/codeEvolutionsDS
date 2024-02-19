4b6355fa9bc031ff16bb8011043425f4248fb718
hudhyfa <hudhyfahbk@gmail.com>
2024-02-19 23:21:31 +0530

    linkedlists on progress

---
+++ linkedList/prepend.js
@@ -0,0 +1,69 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size == 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    prepend(value) {
+        const node = new Node(value);
+        if(this.isEmpty()) {
+            this.head = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size ++;
+    }
+
+    append(value) {
+        const node = new Node(value);
+        if(this.isEmpty()) {
+            this.head = node;
+        } else {
+            let prev = this.head;
+            while(prev.next){
+                prev = prev.next;
+            }
+            prev.next = node;
+        }
+        this.size++;
+    }
+
+    print() {
+        if(this.isEmpty()) {
+            console.log("list is empty");
+        }else{
+            let curr = this.head;
+            let res = "";
+            while(curr) {
+                res += `${curr.value} `;
+                curr = curr.next;
+            }
+            console.log(res);
+        }
+    }
+}
+
+const list = new LinkedList();
+console.log(list.isEmpty());
+console.log(list.getSize());
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.print();
+
